0af5b8ee4d6fbaf74aaaaa36a4bfca91545a2e4f
Wasiewski <oskar.wasiewski@wp.pl>
2020-05-25 00:45:30 +0200

    provide script for building and pushing image  to heroku

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+# build your local Docker image
+docker build -t django-heroku -f Dockerfile .
+
+
+# push your container to heroku with suitable type (web,worker)
+heroku container:push web -a django-heroku-base 
+
+
+# release the web process with your container 
+heroku container:release web -a django-heroku-base 
+
+
+# run migrations
+heroku run python3 manage.py migrate -a django-heroku-base 
