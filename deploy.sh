1daf3d6f34ee186ec17e65050fcaf5d63af2f963
Wasiewski <oskar.wasiewski@wp.pl>
2020-05-25 13:44:12 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,8 @@ heroku container:push web -a django-heroku-base
 # release the web process with your container 
 heroku container:release web -a django-heroku-base 
 
+# test, open application in the browser
+heroku open -a django-heroku-base
 
 # run migrations
 heroku run python3 manage.py migrate -a django-heroku-base 
